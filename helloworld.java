918a3c7ec3377e64a3fe6b8892baa255216544aa
santoshgit89 <101782316+santoshgit89@users.noreply.github.com>
2022-04-07 17:12:24 -0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from GitHub check webhook");
+    System.out.println("Hello World from GitHub check webhook2");
     }
 }
